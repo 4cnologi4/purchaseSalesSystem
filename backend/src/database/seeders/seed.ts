ff7dc21c9235b50ac9050aac867cb1c34c3bfd26
4cnbologi4 <daniell9404@outlook.com>
2025-06-27 13:16:55 -0600

    Add user seeder

---
+++ backend/src/database/seeders/seed.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from "../data-source";
+//import { seedProducts } from "./products.seeder";
+//import { seedUnitOfMeasures } from "./unit-of-measure.seeder";
+//import { seedUsers } from "./user.seeder";
+
+AppDataSource.initialize().then(async () => {
+  console.log("🌱 Ejecutando seeders...");
+  // await seedUsers();
+  // await seedUnitOfMeasures();
+  // await seedProducts();
+  console.log("🌱 Seeding completo ✅");
+  process.exit(0);
+}).catch((err) => {
+  console.error("❌ Error al hacer seed:", err);
+  process.exit(1);
+});
